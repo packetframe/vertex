fd0365769c079f143dd4c1655bea669d9d26e293
Nate Sales <nate@natesales.net>
2022-04-24 23:13:59 -0700

    feat: add working config parser

---
+++ internal/config.go
@@ -0,0 +1,112 @@
+package config
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+const (
+	ActionDeny  = 0
+	ActionAllow = 1
+)
+
+// Filter represents a xdpfw filter policy
+type Filter struct {
+	Enabled          *bool `json:"enabled"`   // Should this rule be enabled?
+	Action           *int  `json:"action"`    // Should the packet be allowed or blocked
+	MinLen           *int  `json:"min_len"`   // Minimum frame length (ethernet header, IP header, L4 header, and data)
+	MaxLen           *int  `json:"max_len"`   // Maximum frame length (ethernet header, IP header, L4 header, and data)
+	PacketsPerSecond *int  `json:"pps"`       // Packets per second that a source IP can send before matching
+	BytesPerSecond   *int  `json:"bps"`       // Bytes per second that a source IP can send before matching
+	BlockTime        *int  `json:"blocktime"` // Time in seconds to block the source IP if the rule matches and the action is block (0). Default value is 1.
+
+	// IP options
+	TypeOfService *int    `json:"tos"`     // IP TOS field
+	SrcIP         *string `json:"srcip"`   // Source IPv4 address
+	DstIP         *string `json:"dstip"`   // Destination IPv4 address
+	SrcIP6        *string `json:"srcip6"`  // Source IPv6 address
+	DstIP6        *string `json:"dstip6"`  // Destination IPv6 address
+	MinTTL        *int    `json:"min_ttl"` // Minimum TTL that the packet must match
+	MaxTTL        *int    `json:"max_ttl"` // Maximum TTL that the packet must match
+
+	// TCP Options
+	TCPEnabled *bool `json:"tcp_enabled"` // Should TCP options be checked?
+	TCPSrcPort *int  `json:"tcp_sport"`   // Source TCP port
+	TCPDstPort *int  `json:"tcp_dport"`   // Destination TCP port
+	TCPFlagURG *bool `json:"tcp_urg"`     // TCP URG flag
+	TCPFlagACK *bool `json:"tcp_ack"`     // TCP ACK flag
+	TCPFlagRST *bool `json:"tcp_rst"`     // TCP RST flag
+	TCPFlagPSH *bool `json:"tcp_psh"`     // TCP PSH flag
+	TCPFlagSYN *bool `json:"tcp_syn"`     // TCP SYN flag
+	TCPFlagFIN *bool `json:"tcp_fin"`     // TCP FIN flag
+
+	// UDP Options
+	UDPEnabled *bool `json:"udp_enabled"` // Should UDP options be checked?
+	UDPSrcPort *int  `json:"udp_sport"`   // Source UDP port
+	UDPDstPort *int  `json:"udp_dport"`   // Destination UDP port
+
+	// ICMP Options
+	ICMPEnabled *bool `json:"icmp_enabled"` // Should ICMP options be checked?
+	ICMPCode    *int  `json:"icmp_code"`    // ICMP code
+	ICMPType    *int  `json:"icmp_type"`    // ICMP type
+}
+
+// String returns a string representation of the filter in xdpfw syntax
+func (f *Filter) String() string {
+	s := "{\n"
+
+	v := reflect.ValueOf(f).Elem()
+	vT := v.Type()
+	for i := 0; i < v.NumField(); i++ {
+		if v.Field(i).IsNil() {
+			continue
+		}
+		value := v.Field(i).Elem().Interface()
+		tag := vT.Field(i).Tag.Get("json")
+
+		// Add quotes to represent empty strings
+		if v.Field(i).Elem().Type() == reflect.TypeOf("") {
+			value = "\"" + v.Field(i).Elem().String() + "\""
+		}
+
+		s += fmt.Sprintf("  %s = %v", tag, value)
+
+		// Add a trialing comma if this is not the last field
+		if v.NumField() != i+1 {
+			s += ","
+		}
+		s += "\n"
+	}
+
+	return s + "}"
+}
+
+type Config struct {
+	Interface  string    `json:"interface"` // Interface to use for the XDP program
+	UpdateTime int       `json:"update_time"`
+	Filters    []*Filter `json:"filters"`
+}
+
+// String converts a Config into a xdpfw config file
+func (c *Config) String() string {
+	s := fmt.Sprintf(`interface = "%s";
+updatetime = %d;
+
+filters = (
+`, c.Interface, c.UpdateTime)
+
+	for i := 0; i < len(c.Filters); i++ {
+		filterStr := c.Filters[i].String()
+		filterStr = strings.ReplaceAll(filterStr, "\n", "\n  ")
+		filterStr = strings.ReplaceAll(filterStr, "{", "  {")
+
+		s += filterStr
+		if i != len(c.Filters)-1 {
+			s += ","
+		}
+		s += "\n"
+	}
+
+	return s + ");\n"
+}
---
+++ internal/config_test.go
@@ -0,0 +1,96 @@
+package config
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPolicyFilterJSON(t *testing.T) {
+	configString := `
+{
+  "interface": "eth0",
+  "update_time": 15,
+  "filters": [
+	{
+	  "action": 0,
+	  "pps": 10,
+	  "bps": 100,
+	  "blocktime": 1,
+	  "tos": 0,
+	  "srcip": "192.0.2.1"
+	}
+  ]
+}
+`
+
+	var config Config
+	err := json.Unmarshal([]byte(configString), &config)
+	assert.Nil(t, err)
+	assert.Equal(t, "eth0", config.Interface)
+	assert.Equal(t, 15, config.UpdateTime)
+	assert.Equal(t, 1, len(config.Filters))
+	assert.Equal(t, 0, *config.Filters[0].Action)
+	assert.Equal(t, 10, *config.Filters[0].PacketsPerSecond)
+	assert.Equal(t, 100, *config.Filters[0].BytesPerSecond)
+	assert.Equal(t, 1, *config.Filters[0].BlockTime)
+	assert.Equal(t, 0, *config.Filters[0].TypeOfService)
+	assert.Equal(t, "192.0.2.1", *config.Filters[0].SrcIP)
+}
+
+func TestPolicyConfigJSON(t *testing.T) {
+	configString := `
+{
+  "interface": "eth0",
+  "update_time": 15,
+  "filters": [
+	{
+	  "name": "test",
+	  "action": 0,
+	  "pps": 10,
+	  "bytes_per_second": 100,
+	  "blocktime": 1,
+	  "tos": 0,
+	  "srcip": "23"
+	},
+	{
+	  "action": 1,
+	  "pps": 10,
+	  "bps": 100,
+	  "blocktime": 1,
+	  "tos": 0,
+	  "srcip": "23"
+	}
+  ]
+}
+`
+
+	var config Config
+	err := json.Unmarshal([]byte(configString), &config)
+	assert.Nil(t, err)
+
+	expected := `interface = "eth0";
+updatetime = 15;
+
+filters = (
+  {
+    action = 0,
+    pps = 10,
+    blocktime = 1,
+    tos = 0,
+    srcip = "23",
+  },
+  {
+    action = 1,
+    pps = 10,
+    bps = 100,
+    blocktime = 1,
+    tos = 0,
+    srcip = "23",
+  }
+);
+`
+
+	assert.Equal(t, expected, config.String())
+}
